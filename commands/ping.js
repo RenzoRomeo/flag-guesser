fe498aeb9b3a944d01ebca71dbbe8dc11cb845a6
Renzo Romeo <renzoromeo44@gmail.com>
2021-09-03 15:51:48 -0300

    Estructura de comandos y eventos

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,12 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+		.addStringOption(option => option
+			.setName('mensaje')
+			.setDescription('Responde con Pong! + mensaje')
+			.setRequired(true)),
 	async execute(interaction) {
-		return interaction.reply('Pong!');
+		return interaction.reply('Pong! ' + interaction.options.getString('input'));
 	},
 };
