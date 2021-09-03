4eb9a2e56a579c9be8572776a05899e6fb3b06a9
Renzo Romeo <renzoromeo44@gmail.com>
2021-09-03 15:21:59 -0300

    Funcionalidad Básica

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('fs');
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
-client.once('ready', () => {
-    console.log("Ready");
-})
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
 
 client.login(token);
